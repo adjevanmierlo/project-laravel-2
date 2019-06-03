973dcbaed6c0437aaf0eca5d24c7efd4344e3281
adjevanmierlo <a.vanmierlo@ziggo.nl>
2019-06-03 12:39:54 +0200

    auth contact aangepast

--- app/Http/Requests/ContactFormRequest.php
+++ app/Http/Requests/ContactFormRequest.php
@@ -6,27 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ContactFormRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
+
     public function rules()
     {
         return [
-            'name' => 'required|max: 255',
+            'name' => 'required|max: 255|required|regex:/^[\pL\s\-]+$/u',
             'email' => 'required|email|max: 255',
-            'message' => 'required',
+            'message' => 'required|min: 10',
         ];
     }
 }
